9d375d25683c1f562afbc5ccde8a42029059574e
RLlopP <rllop@edu.tecnocampus.cat>
2019-06-11 00:48:37 +0200

    5 done

--- specs/5.Objects.spec.js
+++ specs/5.Objects.spec.js
@@ -3,19 +3,19 @@ describe("Objects", () => {
   describe("JSON", () => {
 
     it("{} is an empty object", () => {
-      expect(solveme).toEqual(jasmine.any(Object));
+      expect({}).toEqual(jasmine.any(Object));
     });
 
     it('{hello: "world"} is an object with one field', () => {
-      expect(solveme).toEqual(jasmine.any(Object));
+      expect({hello: "world"}).toEqual(jasmine.any(Object));
     });
 
     it("JSON.parse converts JSON strings to obejcts", () => {
-      expect(solveme).toEqual(JSON.parse('{"a":1,"b":[2,3]}'));
+      expect({a:1,b:[2,3]}).toEqual(JSON.parse('{"a":1,"b":[2,3]}'));
     });
 
     it("JSON.stringify converts any object to JSON", () => {
-      expect(solveme).toEqual(JSON.stringify({b:3}));
+      expect('{"b":3}').toEqual(JSON.stringify({b:3}));
     });
 
   });
@@ -31,32 +31,32 @@ describe("Objects", () => {
     });
 
     it("you can use . to get the value of a property", () => {
-      expect(solveme).toBe(object.all);
+      expect(42).toBe(object.all);
     });
 
     it("you can use [string] to get the value of a property", () => {
-      expect(solveme).toBe(object["hello"]);
+      expect("world").toBe(object["hello"]);
     });
 
     it("you can use . to set the value of a property", () => {
       object.hello = "you";
-      expect(solveme).toEqual(object);
+      expect({hello: "you",all: 42,}).toEqual(object);
     });
 
     it("you can use [string] to set the value of a property", () => {
       object["hello"] = "here";
-      expect(solveme).toEqual(object);
+      expect({hello: "here",all: 42,}).toEqual(object);
     });
 
     it("return undefined if the property is not defined", () => {
-      expect(solveme).toBe(object.foo);
-      expect(solveme).toBe(object["bar"]);
+      expect(undefined).toBe(object.foo);
+      expect(undefined).toBe(object["bar"]);
     });
 
     it("you can add dynamically any property", () => {
       object.foo = "banana";
       object["bar"] = "apple";
-      expect(solveme).toEqual(object);
+      expect({all: 42, bar: "apple", foo: "banana", hello: "world"}).toEqual(object);
     });
 
   });
@@ -72,11 +72,11 @@ describe("Objects", () => {
     });
 
     it("Object.keys() gets an array with all the property keys", () => {
-      expect(solveme).toEqual(Object.keys(object));
+      expect(["hello","all"]).toEqual(Object.keys(object));
     });
 
     it("Object.values() gets an array with all property values", () => {
-      expect(solveme).toEqual(Object.values(object));
+      expect(["world", 42]).toEqual(Object.values(object));
     });
 
     it("Object.keys() can be used to walk all properties of an object", () => {
@@ -87,7 +87,7 @@ describe("Objects", () => {
           result = `${key}=${value}`
         }
       });
-      expect(solveme).toBe(result);
+      expect("hello=world").toBe(result);
     });
 
   });
@@ -109,26 +109,26 @@ describe("Objects", () => {
       let copy = {...salute};
       salute.hello = 'catelyn';
 
-      expect(solveme).toEqual(copy);
+      expect({"hello": "world"}).toEqual(copy);
     });
 
     it("merges objects", () => {
       let merge = {...salute, ...meaning};
 
-      expect(solveme).toEqual(merge);
+      expect({"all": 42, "hello": "world"}).toEqual(merge);
     });
 
     it("last merge prevails", () => {
       let saluteLoras = { hello: 'loras' }
       let merge = { ...salute, ...meaning, ...saluteLoras };
 
-      expect(solveme).toEqual(merge);
+      expect({"all": 42, "hello": "loras"}).toEqual(merge);
     });
 
     it("it can combine with other properties", () => {
       let merge = { ...salute, child: 'joffrey' };
 
-      expect(solveme).toEqual(merge);
+      expect({"child": "joffrey", "hello": "world"}).toEqual(merge);
     });
   });
 
@@ -145,13 +145,13 @@ describe("Objects", () => {
     it("can get a property value", () => {
       let { peter } = main;
 
-      expect(solveme).toEqual(peter);
+      expect("tyrion").toEqual(peter);
     });
 
     it("can remaining an property", () => {
       let { peter, ...rest } = main;
 
-      expect(solveme).toEqual(rest);
+      expect({"kit": "jon"}).toEqual(rest);
     });
 
   });
@@ -168,19 +168,19 @@ describe("Objects", () => {
     it("can get a property value", () => {
       let other = { ['sophie']: 'sansa' };
 
-      expect(solveme).toEqual(other);
+      expect({"sophie": "sansa"}).toEqual(other);
     });
 
     it("can be combined with destructuring", () => {
       let other = { ...main, ['maisie']: 'arya' };
 
-      expect(solveme).toEqual(other);
+      expect({"maisie": "arya", "peter": "tyrion"}).toEqual(other);
     });
 
     it("can be be used to substitute existing elements", () => {
       let other = { ...main, ['peter']: 'arthur' };
 
-      expect(solveme).toEqual(other);
+      expect({"peter": "arthur"}).toEqual(other);
     });
 
   });
@@ -200,7 +200,7 @@ describe("Objects", () => {
         object.foo = "bar";
         delete object.hello;
         delete object["all"];
-        expect(solveme).toEqual(object);
+        expect({foo:"bar"}).toEqual(object);
       });
 
       it("non string keys are converted into string", () => {
